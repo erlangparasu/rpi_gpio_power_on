9c4f468e964cffc1a74290c14b7f6c36250c6cc3
Erlang Parasu <erlangparasu@gmail.com>
2024-09-20 11:01:47 +0800

    chore: comment

--- power_on.py
+++ power_on.py
@@ -5,19 +5,19 @@ import time
 GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
 
 # Define the GPIO pin number
-pin_number = 21 # Change this to your desired GPIO pin number
+pin_number = 21  # Change this to your desired GPIO pin number
 
 # Set up the GPIO pin as an output
 GPIO.setup(pin_number, GPIO.OUT)
 
 try:
-    GPIO.output(pin_number, GPIO.LOW)  # Set pin HIGH
+    GPIO.output(pin_number, GPIO.HIGH)  # Set pin HIGH
     print("Pin is HIGH")
-    time.sleep(1)                        # Wait for 1 second
+    time.sleep(1)  # Wait for 1 second
 
-    GPIO.output(pin_number, GPIO.HIGH)   # Set pin LOW
+    GPIO.output(pin_number, GPIO.LOW)  # Set pin LOW
     print("Pin is LOW")
-    time.sleep(1)                        # Wait for 1 second
+    time.sleep(1)  # Wait for 1 second
 
 except KeyboardInterrupt:
     pass  # Exit the loop on Ctrl+C
