53ca25643913b5d28d4a938ce29a0dfeafff3c93
Erlang Parasu <erlangparasu@gmail.com>
2024-09-23 10:53:43 +0800

    feat: force power off

---
+++ force_power_off.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
+
+# Define the GPIO pin number
+pin_number = 21  # Change this to your desired GPIO pin number
+
+# Set up the GPIO pin as an output
+GPIO.setup(pin_number, GPIO.OUT)
+
+try:
+    GPIO.output(pin_number, GPIO.HIGH)  # Set pin HIGH
+    print("Pin is HIGH")
+    time.sleep(1)  # Wait for 1 second
+
+    GPIO.output(pin_number, GPIO.LOW)  # Set pin LOW
+    print("Pin is LOW")
+    time.sleep(5)  # Wait for 5 second
+
+except KeyboardInterrupt:
+    pass  # Exit the loop on Ctrl+C
+
+finally:
+    GPIO.cleanup()  # Clean up GPIO settings
